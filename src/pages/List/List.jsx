91fc28c7c5ed1f8bed7387b029ee1f31ca26054b
severuna <oxxxyfomenkova@gmail.com>
2023-07-14 23:25:01 +0300

    add views for list item

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './List.css';
+import { recipesStore } from '../../store';
+import ListItem from './ListItem/ListItem';
 
 const List = () => {
+    const recipes = recipesStore((state) => state.recipes);
+    const fetchRecipes = recipesStore((state) => state.fetchRecipes);
+    
+    useEffect(() => {
+        fetchRecipes()
+    }, [fetchRecipes])
+
+    let elements = [];
+
+    recipes.forEach((element) => {
+        elements.push(<ListItem key={element.id} {...element} />)
+    });
+
     return (
         <div className='list row'>
-            <h2>list</h2>
+            {elements}
         </div>
     );
 };
--- src/pages/List/ListItem/ListItem.jsx
+++ src/pages/List/ListItem/ListItem.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ListItem.css';
 
-const ListItem = () => {
+const ListItem = ( props ) => {
+    const [width, setWidth] = useState({});
+
+    const setImageSize = (e) => {
+        e.preventDefault();
+        if(e.target.height <= 888) {
+            setWidth({
+                width: '150px',
+                height: '330px'
+            });
+        } else {
+            setWidth({
+                width: '70px',
+                height: '330px'
+            });
+        }
+    }
+
     return (
-        <div className='list-item column'>
-            list item
+        <div className='list-item row'>
+        <img src={props.image_url} alt={props.name} onLoad={(e) => setImageSize(e)} className='list-item__img' style={width}/>
+            <div className='list-item__content column'>
+                <div className='column'>
+                <h2 className='list-item__title'>{props.name}</h2>
+                <p className='list-item__description'>{props.description.length > 100 ? props.description.substr(0, 200) + '...' : props.description}</p>
+                <p className='list-item__tagline'><span>TAGLINE:</span> {props.tagline}</p>
+                </div>
+                <div className='list-item__details row'>
+                    <p><span>IBU: </span>{props.ibu}</p>
+                    <p><span>ABV: </span>{props.abv}</p>
+                    <p><span>PH: </span>{props.ph}</p>
+                </div>
+            </div>
         </div>
     );
 };
